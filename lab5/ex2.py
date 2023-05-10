484abaa5079cb62f7236f2c32b61509dcd1f0d22
Nimesha Rajapaksha <127377276+NimeshaR@users.noreply.github.com>
2023-05-10 19:57:34 +0530

    final

---
+++ lab5/ex2.py
@@ -0,0 +1,7 @@
+print('A', end='')
+print('B', end='')
+print('C', end='')
+print()
+print('A')
+print('B')
+print('C')
---
+++ lab5/ex3.py
@@ -0,0 +1,11 @@
+a = input('Enter a number:')
+print(a)
+
+
+a=int(input('Enter a number:'))
+print(a)
+
+a=float(input('Enter a number:'))
+print(a)
+
+
